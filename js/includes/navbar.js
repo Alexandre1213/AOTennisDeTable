25998c4b38eb6e08948cb960b298ad7ec367474b
Alexandre1213 <alex.apps.12@gmail.com>
2019-09-15 11:42:52 +0200

    First commit

---
+++ js/includes/navbar.js
@@ -0,0 +1,20 @@
+document.write(`
+<nav>
+    <div class="logo">
+        <h4>Tennis de Table St-Nolff</h4>
+    </div>
+    <ul class="nav-links">
+        <li><a href="./index.html">Accueil</a></li>
+        <li><a href="Compétitions">Compétitions</a></li>
+        <li><a href="./contact.html">Contact</a></li>
+        <li><a href="Acutvueob">Actualités</a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+</nav>
+
+<script src="./js/main.js"></script>
+`)
